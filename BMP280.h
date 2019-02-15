c0e3682d3b6e60611136e2fd068c8860317d71a6
MartinL1 <31612615+MartinL1@users.noreply.github.com>
2019-02-15 16:55:38 +0000

    Update BMP280.h

--- BMP280.h
+++ BMP280.h
@@ -3,7 +3,8 @@
 	
 	Copyright (C) Martin Lindupp 2019
 	
-	V1.0.0 -- Initial release 									
+	V1.0.0 -- Initial release 		
+	
 	The MIT License (MIT)
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
